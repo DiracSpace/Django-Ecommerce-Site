04a927da75d6d32a567700ebd9ac1a781b261d05
DiracSpace <www.ralphlaurenpolo@gmail.com>
2020-11-29 00:27:57 -0800

    functionality for non-logged user

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path, re_path
-from . import views 
+from django.urls import path, re_path, include
+from . import views
 
 urlpatterns = [
     re_path(r'^$', views.Store, name='store'),
--- store/views.py
+++ store/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
-from django.views.decorators.cache import cache_control
+from .models import *
 
 # Create your views here.
-@cache_control(max_age=180)
 def Store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products' : products}
     return render(request, 'store/store.html', context)
 
-@cache_control(max_age=180)
 def Cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context = {'items' : items}
     return render(request, 'store/cart.html', context)
 
-@cache_control(max_age=180)
 def Checkout(request):
     context = {}
     return render(request, 'store/checkout.html', context)
