21e5fc1507d7ac05ad3b2e66fb2c665820750737
DiracSpace <www.ralphlaurenpolo@gmail.com>
2020-11-28 19:26:43 -0800

    added bootstrap design

--- coronasecommerce/urls.py
+++ coronasecommerce/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, re_path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="adminpanel"),
     re_path('', include('store.urls')),
-    
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
